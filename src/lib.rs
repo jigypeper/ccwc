0db0a02e4ca7de9f88bb1d995292833306927c6f
Ahmed Hagi <ahmedosman@mac.com>
2023-04-11 17:07:27 +0100

    wip

---
+++ src/lib.rs
@@ -0,0 +1,117 @@
+use std::{io::{stdin, BufRead}, env, fs};
+use clap::{Parser, Args};
+
+#[derive(Parser)]
+struct Cli {
+    /// number of bytes
+    #[arg(short)]
+    count: String,
+}
+
+pub fn arg_handler(args: Vec<String>) {
+    let mut file = String::from("");
+
+    if args.len() > 1 {
+        file = args.iter()
+            .next().expect("File does not exist").to_string();
+
+        let data = get_stats(&file);
+
+        println!("{} {} {} {} {}", data.bytes, data.characters, data.words, data.lines, file);
+
+    } else if args.len() > 2 {
+        file = args.iter()
+            .next().expect("File does not exist").to_string();
+
+        let data = get_stats(&file); 
+
+        let data_type = &args.iter()
+            .next().unwrap()[..];
+
+        match data_type {
+            "-c" => println!("{} {}", data.characters, file),
+            "-l" => println!("{} {}", data.lines, file),
+            "-w" => println!("{} {}", data.words, file),
+            "-b" => println!("{} {}", data.bytes, file),
+            _ => println!("Not a valid option")
+        }
+
+    } else {
+        let data = get_stats(&stdin().lock().lines().next().unwrap().unwrap()[..]);
+        println!("{} {} {} {} {}", data.bytes, data.characters, data.words, data.lines, file);
+    }
+    
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Count {
+    pub bytes: usize,
+    pub characters: usize,
+    pub words: usize,
+    pub lines: usize
+}
+
+pub fn get_env() -> Vec<String> {
+    let vars: Vec<String> = env::args()
+        .map(|x| x.trim().to_string())
+        .collect();
+    vars
+}
+
+pub fn get_stats(file_path: &str) -> Count {
+    let data = fs::read_to_string(file_path);
+    let mut output = Count {
+        bytes: 0,
+        characters: 0,
+        words: 0,
+        lines: 0,
+    };
+
+    match data {
+        Ok(contents) => {
+            let ref_contents = &contents;
+            output.bytes = ref_contents.len();
+            output.characters = ref_contents.replace("\n", "").chars().count();
+            output.words = ref_contents.split_whitespace().collect::<Vec<&str>>().len();
+            output.lines = ref_contents.lines().collect::<Vec<&str>>().len();
+
+        },
+        Err(_) => eprintln!("Invalid file path")
+    }
+
+    output
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn test_get_env() {
+        assert_eq!(vec!(std::env::current_exe().unwrap().into_os_string().into_string().unwrap()), get_env());
+    }
+
+    #[test]
+    fn test_count_valid() {
+        assert_eq!(Count {
+            bytes: 20,
+            characters: 19,
+            words: 5,
+            lines: 2
+
+        },
+    get_stats("./README.md"));
+    }
+
+    #[test]
+    fn test_count_invalid() {
+        assert_eq!(Count {
+            bytes: 0,
+            characters: 0,
+            words: 0,
+            lines: 0
+
+        },
+    get_stats("./some_non_existent_file.md"));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,27 @@
-use std::{env, fs};
+use std::{path::PathBuf, io::BufReader, fs::File};
 
-#[derive(Debug, PartialEq)]
-struct Count {
-    bytes: usize,
-    characters: usize,
-    words: usize,
-    lines: usize
-}
+use ccwc::{
+    get_env,
+    arg_handler,
+};
 
-fn get_env() -> Vec<String> {
-    let vars: Vec<String> = env::args()
-        .map(|x| x.trim().to_string())
-        .collect();
-    vars
-}
+use is_terminal::IsTerminal as _;
 
-fn arg_handler(arg: &str) {
-    todo!();
-}
-
-fn get_stats(file_path: &str) -> Count {
-    let data = fs::read_to_string(file_path);
-    let mut output = Count {
-        bytes: 0,
-        characters: 0,
-        words: 0,
-        lines: 0,
-    };
-
-    match data {
-        Ok(contents) => {
-            let ref_contents = &contents;
-            output.bytes = ref_contents.len();
-            output.characters = ref_contents.replace("\n", "").chars().count();
-            output.words = ref_contents.split_whitespace().collect::<Vec<&str>>().len();
-            output.lines = ref_contents.lines().collect::<Vec<&str>>().len();
-
-        },
-        Err(_) => eprintln!("Invalid file path")
-    }
-
-    output
-}
 
 fn main() {
-    todo!();
-}
+    let args = get_env();
+    let _stats = arg_handler(args);
 
-#[cfg(test)]
-mod tests {
-    use crate::*;
+    let word_count;
+    let mut file = args.file;
 
-    #[test]
-    fn test_get_env() {
-        assert_eq!(vec!(std::env::current_exe().unwrap().into_os_string().into_string().unwrap()), get_env());
+    if file == PathBuf::from("-") {
+
+        file = PathBuf::from("<stdin>");
+        word_count = words_in_buf_reader(BufReader::new(stdin().lock()));
+    } else {
+        word_count = words_in_buf_reader(BufReader::new(File::open(&file).unwrap()));
     }
 
-    #[test]
-    fn test_count_valid() {
-        assert_eq!(Count {
-            bytes: 20,
-            characters: 19,
-            words: 5,
-            lines: 2
-
-        },
-    get_stats("./README.md"));
-    }
-
-    #[test]
-    fn test_count_invalid() {
-        assert_eq!(Count {
-            bytes: 0,
-            characters: 0,
-            words: 0,
-            lines: 0
-
-        },
-    get_stats("./some_non_existent_file.md"));
-    }
+    println!("Words from {}: {}", file.to_string_lossy(), word_count)
 }
